51810bab51885dccab2db328b93cd12f2a6834b5
Victor Gabriel <69489641+Vitao5@users.noreply.github.com>
2026-06-23 18:25:09 -0300

    Add files via upload

---
+++ src/main/java/dev/junio/queue/exercises/Exer01ShiftPop.java
@@ -0,0 +1,11 @@
+package dev.junio.queue.exercises;
+
+import dev.junio.queue.arrayqueue.ArrayQueue;
+
+public class Exer01ShiftPop<T> extends ArrayQueue<T> {
+
+
+    public Exer01ShiftPop(int capacity) {
+        super(capacity);
+    }
+}
---
+++ src/main/java/dev/junio/queue/exercises/Exer04CountOccurrences.java
@@ -0,0 +1,31 @@
+package dev.junio.queue.exercises;
+
+import dev.junio.queue.linkedqueue.LinkedQueue;
+
+public class Exer04CountOccurrences<T> {
+
+    public static int countOccurrences(LinkedQueue<Integer> queue, int target) {
+
+        LinkedQueue<Integer> listrest = new LinkedQueue<>();
+        int count = 0;
+
+        while (!queue.isEmpty()) {
+            Integer current = queue.pop();
+
+
+            if (current != null && current == target) {
+                count++;
+            }
+
+
+            listrest.push(current);
+        }
+
+        while (!listrest.isEmpty()){
+            queue.push(listrest.pop());
+        }
+
+
+        return count;
+    }
+}
